2360439d08ab9661554840e03e47adb1eb20cd87
sherevan93 <108557020+sherevan93@users.noreply.github.com>
2022-07-11 16:34:10 -0400

    Update HelloWorld1.java

--- Day2/HelloWorld1.java
+++ Day2/HelloWorld1.java
@@ -3,6 +3,6 @@ package Day2;
 public class HelloWorld1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World 2");
+        System.out.println("Hello World 2 day");
     }
 }
